901820f1509320981552517add597f3af7ad07d7
Kire008 <eriksomm@afk.no>
2025-09-16 11:38:40 +0200

    Script change

--- Files/script.js
+++ Files/script.js
@@ -1,6 +1,12 @@
+function randInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 function LagSpørsmål() {
     let tall1 = randInt(0, 9);
-    let tall2 = randInt(0,tall2);
+    let tall2 = randInt(0,10-tall1);
+    console.log(tall1,tall2);
 
-    return tall1+tall2;
-}
+    return tall1,tall2;
+};
+
